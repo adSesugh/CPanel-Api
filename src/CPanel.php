a9c389a6c16102b9e3eeed4285905e4ffc60903d
KeennessSolutions <sesughagbadu@yahoo.com>
2021-03-29 19:17:40 +0100

    config changes

--- src/CPanel.php
+++ src/CPanel.php
@@ -19,7 +19,7 @@ class CPanel
     public function __construct($cpanel_domain=null, $cpanel_api_token=null, $cpanel_username=null, $protocol='https', $port=2083)
     {
 
-        $this->config = Config::get('cpanelapi');
+        $this->config = Config::get('config');
 
         if(isset($cpanel_domain))
         {
